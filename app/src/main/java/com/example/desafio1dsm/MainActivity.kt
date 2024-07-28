d82fe28c38ed7cab7083c8670978f9bbe7f630f3
Fernando Urrutia <148740801+Ghost017890@users.noreply.github.com>
2024-07-27 22:08:38 -0600

    se agrega el proyecto

---
+++ app/src/main/java/com/example/desafio1dsm/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.desafio1dsm
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.desafio1dsm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), TaskItemClickListener
+{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener{
+            NewTaskSheet(null).show(supportFragmentManager, "nuevaEtiquetaTarea")
+        }
+
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView()
+    {
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListRecyclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem)
+    {
+        NewTaskSheet(taskItem).show(supportFragmentManager,"nuevaEtiquetaTarea")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem)
+    {
+        taskViewModel.setCompleted(taskItem)
+    }
+}
---
+++ app/src/main/java/com/example/desafio1dsm/TaskItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.desafio1dsm
+
+interface TaskItemClickListener
+{
+    fun editTaskItem(taskItem: TaskItem)
+    fun completeTaskItem(taskItem: TaskItem)
+}
